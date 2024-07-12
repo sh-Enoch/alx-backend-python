9f5c3b3486e77a5c29b806c9f90cf9563b4f559e
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-12 22:19:11 +0000

    <created> function: wait_n

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Multi coroutines."""
 import asyncio
-import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Retrun list of all delays."""
-    ls = []
+    ls: List[float] = []
     for obj in range(n):
         new_values = await wait_random(max_delay)
         ls.append(new_values)
