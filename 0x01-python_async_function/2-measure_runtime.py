7d62aec9981a36520dda963744f6e44600214f75
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-12 22:44:07 +0000

    <created> function: measure_time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Measure time."""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure execution time of wait_n."""
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter() - start
+
+    return end / n
