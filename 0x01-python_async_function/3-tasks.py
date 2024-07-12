2a5c8bf2dddbabe2f3dccdc598a37af1ae3f90d4
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-12 23:17:23 +0000

    <create> function : task_wait_random

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,7 +4,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Task[float]:
     """Regular function that returns a coroutine."""
     t1 = asyncio.create_task(wait_random(max_delay))
     return t1
