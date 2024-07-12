a34706b78f173e1ce8c9e90447995a42e28850f7
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-12 23:42:30 +0000

    <created> function: task_wait_n

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Multi coroutines."""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Return a list of all delays."""
+    ls = [task_wait_random(max_delay) for i in range(n)]
+    return [await task for task in asyncio.as_completed(ls)]
