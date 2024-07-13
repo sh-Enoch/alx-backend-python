1fe6d03df2b4a3056aaea2d4b47e42fbbe8d7adc
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-13 00:08:49 +0000

    <created> function:measure_runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,10 +6,10 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """Execute async_comprehension 4 times and measure time."""
     start = time.perf_counter()
     t = async_comprehension()
     await asyncio.gather(t, t, t, t)
     end = time.perf_counter() - start
-    retur:n end
+    return end
